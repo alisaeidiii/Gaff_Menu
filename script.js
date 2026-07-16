46ef208731d5812656ab77ab0be6a992d992a14c
alisaeidiii <asaeedi051hhshhs@gmail.com>
2026-07-16 13:49:22 +0330

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,22 @@ btn.onclick = () => {
     behavior: "smooth"
   });
 };
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", function(){
+
+    let value = this.value.toLowerCase();
+
+    document.querySelectorAll(".card").forEach(card=>{
+
+        let text = card.innerText.toLowerCase();
+
+        if(text.includes(value)){
+            card.style.display="block";
+        }else{
+            card.style.display="none";
+        }
+
+    });
+
+});
